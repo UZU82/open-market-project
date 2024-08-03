d961c3e4fcf0bb6bf8a3703c632674e828ad3403
jang.yuju <ujoo2802@gmail.com>
2024-08-03 22:25:20 +0900

    코드 추가

--- js/index.js
+++ js/index.js
@@ -3,17 +3,23 @@ const $ul = document.querySelector('#productList'); // ID 선택자 사용
 function drawProduct(dataItem) {
     const $li = document.createElement('li'); // div 대신 li 사용
     const $h2 = document.createElement('h2'); // h1 대신 h2 사용
-    const $p = document.createElement('p');
+    const $p1 = document.createElement('p');
+    const $p2 = document.createElement('p');
     const $img = document.createElement('img');
 
     $img.src = dataItem.image;
     $img.alt = dataItem.product_name; // 접근성을 위한 alt 속성 추가
     $h2.textContent = dataItem.product_name;
-    $p.textContent = `가격: ${dataItem.price.toLocaleString()}원`;
+    $p1.textContent = `${dataItem.price.toLocaleString()}원`;
+    $p2.textContent = dataItem.store_name;
+
+    $p1.className = 'p1';
+    $p2.className = 'p2';
 
     $li.appendChild($img);
+    $li.appendChild($p2);
     $li.appendChild($h2);
-    $li.appendChild($p);
+    $li.appendChild($p1);
     $ul.appendChild($li);
 }
 
