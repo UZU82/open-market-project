162418140d829c5a48014e834ccee64c64b7b2b9
jang.yuju <ujoo2802@gmail.com>
2024-08-04 20:01:07 +0900

    코드 정리

--- js/index.js
+++ js/index.js
@@ -1,40 +1,48 @@
-const $ul = document.querySelector('#productList'); // ID 선택자 사용
+const ProductList = document.querySelector('#product-list'); // ID 선택자 사용
 
 function drawProduct(dataItem) {
-    const $li = document.createElement('li');
-    const $h2 = document.createElement('h2');
-    const $p1 = document.createElement('p');
-    const $p2 = document.createElement('p');
-    const $img = document.createElement('img');
-    const $span = document.createElement('span');
+    const productListItem = document.createElement('li');
+    const productLink = document.createElement('a');
+    const productName = document.createElement('h2');
+    const productPrice = document.createElement('p');
+    const storeName = document.createElement('p');
+    const productImg = document.createElement('img');
+    const productPriceUnit = document.createElement('span');
+    const formatPrice = (price) => {
+        return new Intl.NumberFormat('ko-KR').format(price);
+    };
+    // Intl.NumberFormat은 JavaScript의 내장 객체이며, 언어에 맞는 숫자 서식을 지원함
+    // 'ko-KR'은 한국식 숫자 표기법을 사용하겠다는 뜻임
+    // .format(price)는 주어진 숫자를 지정된 로케일에 맞게 형식화하게 됨
 
-    $img.src = dataItem.image;
-    $img.alt = dataItem.product_name;
-    $h2.textContent = dataItem.product_name;
-    $p1.textContent = dataItem.price;
-    $p2.textContent = dataItem.store_name;
-    $span.textContent = '원';
+    productImg.src = dataItem.image;
+    productImg.alt = dataItem.product_name;
+    storeName.textContent = dataItem.store_name;
+    productName.textContent = dataItem.product_name;
+    productPrice.textContent = formatPrice(dataItem.price);
+    productPriceUnit.textContent = '원';
 
-    $p1.className = 'p1';
-    $p2.className = 'p2';
+    productImg.className = 'product-img';
+    storeName.className = 'store-name';
+    productName.className = 'product-name';
+    productPrice.className = 'product-price';
+    productPriceUnit.className = 'product-price-unit';
 
-    $li.appendChild($img);
-    $li.appendChild($p2);
-    $li.appendChild($h2);
-    $p1.appendChild($span);
-    $li.appendChild($p1);
-    $ul.appendChild($li);
+    productLink.appendChild(productImg);
+    productLink.appendChild(storeName);
+    productLink.appendChild(productName);
+    productPrice.appendChild(productPriceUnit);
+    productLink.appendChild(productPrice);
+    productListItem.appendChild(productLink);
+    ProductList.appendChild(productListItem);
 }
 
 fetch('https://openmarket.weniv.co.kr/products')
     .then((res) => {
-        if (!res.ok) {
-            throw new Error('네트워크 응답이 올바르지 않습니다');
-        }
         return res.json();
     })
     .then((data) => {
-        data.results.forEach(drawProduct); // data.results 사용
+        data.results.forEach(drawProduct);
     })
     .catch((error) => {
         console.error('데이터를 가져오는 중 오류 발생:', error);
